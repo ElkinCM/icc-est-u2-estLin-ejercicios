1a11c4c17db69ebaa037b3f638f1efa75d2c958f
ElkinCM <elkinmaura@gmail.com>
2025-06-24 08:18:12 -0500

    Ejercicios Resueltos

--- src/main/LogicaClasificacion.java
+++ src/main/LogicaClasificacion.java
@@ -15,7 +15,16 @@ public class LogicaClasificacion {
      *         Salida: "odnuM aloH"
      */
     public String invertirCadena(String texto) {
-        return "";
+        Stack<Character> pila = new Stack<>();
+        for (char c : texto.toCharArray()) {
+            pila.push(c);
+        }
+
+        StringBuilder invertido = new StringBuilder();
+        while (!pila.isEmpty()) {
+            invertido.append(pila.pop());
+        }
+        return invertido.toString();
     }
 
     /**
@@ -30,7 +39,21 @@ public class LogicaClasificacion {
      *         Salida: true
      */
     public boolean validarSimbolos(String expresion) {
-        return false;
+        Stack<Character> pila = new Stack<>();
+        Map<Character, Character> pares = new HashMap<>();
+        pares.put(')', '(');
+        pares.put(']', '[');
+        pares.put('}', '{');
+        for (char c : expresion.toCharArray()) {
+            if (pares.containsValue(c)) {
+                pila.push(c);
+            } else if (pares.containsKey(c)) {
+                if (pila.isEmpty() || pila.pop() != pares.get(c)) {
+                    return false;
+                }
+            }
+        }
+        return pila.isEmpty();
     }
 
     /**
@@ -43,12 +66,24 @@ public class LogicaClasificacion {
      *         Salida: [1, 2, 3, 4]
      */
     public List<Integer> ordenarPila(Stack<Integer> pila) {
+        Stack<Integer> pilaAuxiliar = new Stack<>();
+        while (!pila.isEmpty()) {
+            int temp = pila.pop();
+            while (!pilaAuxiliar.isEmpty() && pilaAuxiliar.peek() > temp) {
+                pila.push(pilaAuxiliar.pop());
+            }
+            pilaAuxiliar.push(temp);
+        }
 
-        return new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
+        while (!pilaAuxiliar.isEmpty()) {
+            result.add(pilaAuxiliar.pop());
+        }
+        return result;
     }
 
     /**
-     * Clasifica una lista de enteros separando pares e impares.
+     * Clasifica una lista de enteross separando pares e impares.
      * Usa LinkedList para mantener el orden de inserción.
      *
      * @return Lista con pares primero, luego impares.
@@ -58,6 +93,21 @@ public class LogicaClasificacion {
      *         Salida: [2, 4, 6, 1, 3, 5]
      */
     public List<Integer> clasificarPorParidad(LinkedList<Integer> original) {
+        List<Integer> pares = new LinkedList<>();
+        List<Integer> impares = new LinkedList<>();
+
+        for(int numero : original) {
+            if (numero % 2 == 0) {
+                pares.add(numero);
+            }else{
+                impares.add(numero);
+            }
+        }
+
+        List<Integer> result = new LinkedList<>();
+        result.addAll(pares);
+        result.addAll(impares);
+
 
         return new ArrayList<>();
     }
